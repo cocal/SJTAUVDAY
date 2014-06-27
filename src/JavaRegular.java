5fba1de9c7c6ae00e3f428927a44094031a32a9b
cocal <cocaltzq@gmail.com>
2014-06-28 00:07:15 +0800

    stu

---
+++ src/JavaRegular.java
@@ -0,0 +1,36 @@
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/****
+ * 学好正则表达式 走遍天下都不怕。
+ * @author cocal
+ *
+ */
+public class JavaRegular {
+	
+	/**
+	 * 开头字母
+	 */
+	private static void test1(String desStr){
+		System.out.println("Str : " +  desStr);
+		String rex = "[ABC]";
+		Pattern p = Pattern.compile(rex);
+		Matcher m = p.matcher(desStr);
+		if(m.find()){
+			System.out.println("Done!");
+		}else{
+			System.out.println("HEHE");
+		}
+	}
+	
+	public static void main(String[] argv){
+		//run
+		String[] testStr = {"Abka","Jpad"};
+		for(String s : testStr){
+			JavaRegular.test1(s);
+		}
+	}
+}
