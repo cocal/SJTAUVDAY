dfbd1af84612fca478c6004111468105c99cd8b6
cocal <cocaltzq@gmail.com>
2014-06-28 22:52:02 +0800

    ~~

--- src/JavaRegular.java
+++ src/JavaRegular.java
@@ -27,7 +27,7 @@ public class JavaRegular {
 	}
 	
 	
-	public static void testRex(String desStr, String pattern){
+	/*public static void testRex(String pattern,String desStr){
 		String info = String.format("desStr : [%s] || rex : [%s]", desStr,pattern);
 		System.out.println(info);
 		String rex = pattern;
@@ -38,12 +38,36 @@ public class JavaRegular {
 		}else{
 			System.out.println("HEHE");
 		}
+	}*/
+	public static void testRex(String pattern,String... desStr){
+	//	String info = String.format("desStr : [%s] || rex : [%s]", desStr,pattern);
+		//System.out.println(info);
+		System.out.println("\n---new Test");
+		String rex = pattern;
+		Pattern p = Pattern.compile(rex);
+		for(String s : desStr){
+			String info = String.format("desStr : [%s] || rex : [%s]", s,pattern);
+			System.out.println(info);
+			Matcher m = p.matcher(s);
+			if (m.find()) {
+				System.out.println("Done!");
+			} else {
+				System.out.println("HEHE");
+			}
+		}
 	}
 	public static void main(String[] argv){
 		//run
+		System.out.println("^ 以什么开头 ---居然能匹配'\\n'");
 		String[] testStr = {"Abka","Jpad","Bc"};
 		for(String s : testStr){
-			JavaRegular.testRex(s,"^Ab");
+			JavaRegular.testRex("^Ab",s);
 		}
+		
+		System.out.println(" *匹配前面的子表达式零次或者多次  ");
+		testRex("a*","aa","cc","()");
+		testRex("a+","aa","cc","()");
+		
+		testRex("\\(\\+ \\d \\d\\)+", "(+ 1 2 9)");
 	}
 }
