251212eca8be6b559129799f3f1f3f527ded85d6
cocal <cocaltzq@gmail.com>
2014-08-12 23:48:20 +0800

    OOM

--- src/TestGroup.java
+++ src/TestGroup.java
@@ -5,12 +5,13 @@ import java.util.regex.Pattern;
 public class TestGroup {
 	
 	public static void test1(){
-		String str = "(+ 1 2)";
+		String str = "(+ 1 2 (+ 2 2 ))";
 		Pattern p = Pattern.compile("\\((.*?)\\)");
 		Matcher m = p.matcher(str);
 		m.find();
 		System.out.println(m.group());
 		System.out.println(m.group(1));
+		System.out.println(m.groupCount());
 	}
 	
 	public static void main(String[] argv){
