6ef01a8c63ace2793458ec175d09d5ab685d6102
cocal <cocaltzq@gmail.com>
2014-08-16 00:53:35 +0800

    白瞎了写了这么多年程序 - -

--- src/poj/poj1002.java
+++ src/poj/poj1002.java
@@ -1,5 +1,9 @@
 package poj;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * 企业电话号码转换
  * @author cocal
@@ -7,7 +11,7 @@ package poj;
  */
 public class poj1002 {
 	static int  mapDict[] = {2,2,2,3,3,3, 4,4,4, 5,5,5, 6,6,6, 7,-1,7,7, 8,8,8, 9,9,9,-1};
-	static String[] phones = new String[100000];
+	static Map<String,Integer> phonesAndNum = new HashMap<String,Integer>();
 	
 	public static String StandNos(String phoneNo){
 		String standPhone = "";
@@ -37,5 +41,20 @@ public class poj1002 {
 	
 	public static void main(String args[]) {
 		System.out.println(StandNos("123-AB"));
+		Scanner cin = new Scanner(System.in);
+		int count = cin.nextInt();
+		for(int k=0 ; k<count ; k++){
+			String temp = cin.nextLine();
+			temp = StandNos(temp);
+			if(phonesAndNum.containsKey(temp)){
+				Integer m = phonesAndNum.get(temp);
+				phonesAndNum.put(temp, ++m);
+			}else
+				phonesAndNum.put(temp, 1);			
+		}
+		
+		for(String str : phonesAndNum.keySet()){
+			System.out.println(str + " " + phonesAndNum.get(str));//缺少 按 val值排序
+		}
 	}
 }
